f05120c2332bfc8a59815e10f2b52a422a4e4262
Thiwanon Chomcharoen <gmgame.1525@gmail.com>
2022-04-27 22:25:53 +0700

    :hammer: change k8s config to config file

--- versions.tf
+++ versions.tf
@@ -24,26 +24,16 @@ terraform {
 }
 
 provider "kubectl" {
-  load_config_file       = false
-  host                   = kind_cluster.this.endpoint
-  client_certificate     = kind_cluster.this.client_certificate
-  client_key             = kind_cluster.this.client_key
-  cluster_ca_certificate = kind_cluster.this.cluster_ca_certificate
+  config_path = "${var.cluster_name}-config"
 }
 
 provider "kubernetes" {
-  host                   = kind_cluster.this.endpoint
-  client_certificate     = kind_cluster.this.client_certificate
-  client_key             = kind_cluster.this.client_key
-  cluster_ca_certificate = kind_cluster.this.cluster_ca_certificate
+  config_path = "${var.cluster_name}-config"
 }
 
 provider "helm" {
   kubernetes {
-    host                   = kind_cluster.this.endpoint
-    client_certificate     = kind_cluster.this.client_certificate
-    client_key             = kind_cluster.this.client_key
-    cluster_ca_certificate = kind_cluster.this.cluster_ca_certificate
+    config_path = "${var.cluster_name}-config"
   }
 }
 
